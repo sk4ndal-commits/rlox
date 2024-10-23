8c4335b36245c33bea83b660aa1fa96f6b262bb0
sk4ndal-commits <sascharoggatz1999@outlook.de>
2024-10-23 23:29:01 +0200

    Handle errors in interpreter run method

--- src/interpreter/interpreter.rs
+++ src/interpreter/interpreter.rs
@@ -10,10 +10,14 @@ impl Interpreter {
         Interpreter {}
     }
     
-    pub fn run(&mut self, file_path: &Path) -> Vec<Token> {
-        let source_code = fs::read_to_string(file_path).unwrap();
-        let tokens = self.scan(&source_code).unwrap();
-        tokens
+    pub fn run(&mut self, file_path: &Path) -> Result<Vec<Token>, String> {
+        let source_code = fs::read_to_string(file_path).map_err(|err| {
+            format!("Error reading file {}: {}", file_path.to_string_lossy(), err)
+        })?;
+        let tokens = self.scan(&source_code).map_err(|err| {
+            format!("Error scanning tokens: {}.", err.to_string())
+        })?;
+        Ok(tokens)
     }
     
     fn scan<'a>(&mut self, source_code: &'a str) -> Result<Vec<Token>, &'a str> {
--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,13 @@ use crate::interpreter::interpreter::Interpreter;
 
 fn main() {
     
-    /*
-    let args: Vec<String> = env::args().collect();
-    
-    if args.len() < 2 { 
-        println!("Please specify a filename");
-        return;
-    }
-    
-    println!("Running {:?}", args[1]);
-     */
-    
     let file_name = "samples/Hello.rlox";
     
     println!("Running, {}!", file_name);
     
     let mut interpreter = Interpreter::new();
-    let chars = interpreter.run(&file_name.as_ref());
-    
-    println!("Tokens: {:?}", chars);
+    match interpreter.run(&file_name.as_ref()) {
+        Ok(_tokens) => println!("{:#?}", _tokens),
+        Err(error) => println!("Error: {}", error),
+    }
 }
